e8014764e665e606be3af6e7188e3fa59cd9e428
HunasimaradAbdulBari <sanoof151@gmail.com>
2025-07-08 16:53:50 +0530

    new

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 
 // Database connection
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
@@ -24,5 +24,5 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
